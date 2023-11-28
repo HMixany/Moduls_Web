f2a028a0efac09b6917c7b8c819eb7db49e6bc51
HMixany <137229468+HMixany@users.noreply.github.com>
2023-11-28 21:15:31 +0200

    2nd commit

--- Modul _1_2/kurs.py
+++ Modul _1_2/kurs.py
@@ -41,3 +41,44 @@ if __name__ == '__main__':
     pretty_view(data_adapter(data))
     # data = api_client.get_data('https://api.monobank.ua/bank/currency')
     # pretty_view(data_adapter(data))
+
+s = {
+    'date': '01.12.2014',
+    'bank': 'PB',
+    'baseCurrency': 980,
+    'baseCurrencyLit': 'UAH',
+    'exchangeRate': [
+        {'baseCurrency': 'UAH', 'currency': 'AUD', 'saleRateNB': 12.831925, 'purchaseRateNB': 12.831925},
+        {'baseCurrency': 'UAH', 'currency': 'CAD', 'saleRateNB': 13.21074, 'purchaseRateNB': 13.21074, 'saleRate': 15.0, 'purchaseRate': 13.0},
+        {'baseCurrency': 'UAH', 'currency': 'CZK', 'saleRateNB': 0.679695, 'purchaseRateNB': 0.679695, 'saleRate': 0.8, 'purchaseRate': 0.6},
+        {'baseCurrency': 'UAH', 'currency': 'DKK', 'saleRateNB': 2.525893, 'purchaseRateNB': 2.525893},
+        {'baseCurrency': 'UAH', 'currency': 'HUF', 'saleRateNB': 0.0612592, 'purchaseRateNB': 0.0612592},
+        {'baseCurrency': 'UAH', 'currency': 'ILS', 'saleRateNB': 3.862738, 'purchaseRateNB': 3.862738, 'saleRate': 4.5, 'purchaseRate': 3.7},
+        {'baseCurrency': 'UAH', 'currency': 'JPY', 'saleRateNB': 0.1272593, 'purchaseRateNB': 0.1272593, 'saleRate': 0.15, 'purchaseRate': 0.12},
+        {'baseCurrency': 'UAH', 'currency':'LVL', 'saleRateNB': 0.1272593, 'purchaseRateNB': 0.1272593},
+        {'baseCurrency': 'UAH', 'currency': 'LTL', 'saleRateNB': 5.443385, 'purchaseRateNB': 5.443385},
+        {'baseCurrency': 'UAH', 'currency': 'NOK', 'saleRateNB': 2.160957, 'purchaseRateNB': 2.160957, 'saleRate': 2.6, 'purchaseRate': 2.1},
+        {'baseCurrency': 'UAH', 'urrncy': 'SKK', 'saleRateNB': 2.160957, 'purchaseRateNB': 2.160957},
+        {'baseCurrency': 'UAH', 'currency': 'SEK', 'saleRateNB': 2.028375, 'purchaseRateNB': 2.028375},
+        {'baseCurrency': 'UAH', 'currency': 'CHF', 'saleRateNB': 15.638975, 'purchaseRateNB': 15.638975, 'saleRate': 17.0, 'purchaseRate': 15.5},
+        {'baseCurrency': 'UAH', 'currency': 'GBP', 'saleRateNB': 23.632491, 'purchaseRateNB': 23.632491, 'saleRate': 25.8, 'purchaseRate': 24.0},
+        {'baseCurrency': 'UAH', 'currency': 'USD', 'saleRateNB': 15.056413, 'purchaseRateNB': 15.056413, 'saleRate': 15.7, 'purchaseRate': 15.35},
+        {'baseCurrency': 'UAH', 'currency': 'BYR', 'saleRateNB': 0.00139, 'purchaseRateNB': 0.00139},
+        {'baseCurrency': 'UAH', 'currency': 'EUR', 'saleRateNB': 18.79492, 'purchaseRateNB': 18.79492, 'saleRate': 20.0, 'purchaseRate': 19.2},
+        {'baseCurrency': 'UAH', 'currency': 'GEL', 'saleRateNB': 8.150089, 'purchaseRateNB': 8.150089},
+        {'baseCurrency': 'UAH', 'currency': 'PLZ', 'saleRateNB': 4.492201, 'purchaseRateNB': 4.492201, 'saleRate': 5.0, 'purchaseRate': 4.2}
+    ]
+}
+print([el.get('currency') for el in s['exchangeRate']])
+ext = filter(lambda x: x.get('currency') == 'USD', s['exchangeRate'])
+print(list(ext)[0])
+
+
+def data_adapter2(data: dict, currency: str):
+    dict = list(filter(lambda x: x.get('currency') == currency, data['exchangeRate']))[0]
+    result = {'sale': dict.get('saleRate'), 'purchase': dict.get('purchaseRate')}
+    return result
+
+
+new_s = {s['date']: {'EUR': data_adapter2(s, 'EUR'), 'USD': data_adapter2(s, 'USD')}}
+print(new_s)
--- Modul_5_2/07_client_httpx.py
+++ Modul_5_2/07_client_httpx.py
@@ -1,6 +1,8 @@
+import sys
 import httpx
 import asyncio
 import platform
+from datetime import datetime, timedelta
 
 
 class HttpError(Exception):
@@ -9,7 +11,7 @@ class HttpError(Exception):
 
 async def request(url: str):
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
+        r = await client.get(url, timeout=20)
         if r.status_code == 200:
             result = r.json()
             return result
@@ -17,10 +19,53 @@ async def request(url: str):
             raise HttpError(f"Error status: {r.status_code} for {url}")
 
 
-async def main():
+async def choise(data: dict, currency: str):
+    await asyncio.sleep(0)
+    dict = list(filter(lambda x: x.get('currency') == currency, data['exchangeRate']))[0]
+    return {'sale': dict.get('saleRate'), 'purchase': dict.get('purchaseRate')}
+
+
+async def data_adapter(data: dict):
+    eur_dict = await choise(data, 'EUR')
+    usd_dict = await choise(data, 'USD')
+    return {data['date']: {'EUR': eur_dict, 'USD': usd_dict}}
+
+
+async def main(delta_day):
+    result = []
+    today = datetime.now()
+    current_day = today - timedelta(days=int(delta_day))
     try:
-        respons = await request('https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014')
-        return respons
+        while current_day != today:
+            shift = current_day.strftime("%d.%m.%Y")
+            respons = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={shift}')
+#             respons = s = {'date': '01.12.2014','bank': 'PB','baseCurrency': 980,'baseCurrencyLit': 'UAH','exchangeRate': [
+#         {'baseCurrency': 'UAH', 'currency': 'AUD', 'saleRateNB': 12.831925, 'purchaseRateNB': 12.831925},
+#         {'baseCurrency': 'UAH', 'currency': 'CAD', 'saleRateNB': 13.21074, 'purchaseRateNB': 13.21074, 'saleRate': 15.0, 'purchaseRate': 13.0},
+#         {'baseCurrency': 'UAH', 'currency': 'CZK', 'saleRateNB': 0.679695, 'purchaseRateNB': 0.679695, 'saleRate': 0.8, 'purchaseRate': 0.6},
+#         {'baseCurrency': 'UAH', 'currency': 'DKK', 'saleRateNB': 2.525893, 'purchaseRateNB': 2.525893},
+#         {'baseCurrency': 'UAH', 'currency': 'HUF', 'saleRateNB': 0.0612592, 'purchaseRateNB': 0.0612592},
+#         {'baseCurrency': 'UAH', 'currency': 'ILS', 'saleRateNB': 3.862738, 'purchaseRateNB': 3.862738, 'saleRate': 4.5, 'purchaseRate': 3.7},
+#         {'baseCurrency': 'UAH', 'currency': 'JPY', 'saleRateNB': 0.1272593, 'purchaseRateNB': 0.1272593, 'saleRate': 0.15, 'purchaseRate': 0.12},
+#         {'baseCurrency': 'UAH', 'currency':'LVL', 'saleRateNB': 0.1272593, 'purchaseRateNB': 0.1272593},
+#         {'baseCurrency': 'UAH', 'currency': 'LTL', 'saleRateNB': 5.443385, 'purchaseRateNB': 5.443385},
+#         {'baseCurrency': 'UAH', 'currency': 'NOK', 'saleRateNB': 2.160957, 'purchaseRateNB': 2.160957, 'saleRate': 2.6, 'purchaseRate': 2.1},
+#         {'baseCurrency': 'UAH', 'urrncy': 'SKK', 'saleRateNB': 2.160957, 'purchaseRateNB': 2.160957},
+#         {'baseCurrency': 'UAH', 'currency': 'SEK', 'saleRateNB': 2.028375, 'purchaseRateNB': 2.028375},
+#         {'baseCurrency': 'UAH', 'currency': 'CHF', 'saleRateNB': 15.638975, 'purchaseRateNB': 15.638975, 'saleRate': 17.0, 'purchaseRate': 15.5},
+#         {'baseCurrency': 'UAH', 'currency': 'GBP', 'saleRateNB': 23.632491, 'purchaseRateNB': 23.632491, 'saleRate': 25.8, 'purchaseRate': 24.0},
+#         {'baseCurrency': 'UAH', 'currency': 'USD', 'saleRateNB': 15.056413, 'purchaseRateNB': 15.056413, 'saleRate': 15.7, 'purchaseRate': 15.35},
+#         {'baseCurrency': 'UAH', 'currency': 'BYR', 'saleRateNB': 0.00139, 'purchaseRateNB': 0.00139},
+#         {'baseCurrency': 'UAH', 'currency': 'EUR', 'saleRateNB': 18.79492, 'purchaseRateNB': 18.79492, 'saleRate': 20.0, 'purchaseRate': 19.2},
+#         {'baseCurrency': 'UAH', 'currency': 'GEL', 'saleRateNB': 8.150089, 'purchaseRateNB': 8.150089},
+#         {'baseCurrency': 'UAH', 'currency': 'PLZ', 'saleRateNB': 4.492201, 'purchaseRateNB': 4.492201, 'saleRate': 5.0, 'purchaseRate': 4.2}
+#     ]
+# }
+            result.append(await data_adapter(respons))
+            current_day = current_day + timedelta(days=1)
+        return result
+        # respons = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014')
+        # return respons
     except HttpError as err:
         print(err)
 
@@ -28,5 +73,5 @@ async def main():
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
+    r = asyncio.run(main(sys.argv[1]))
     print(r)
