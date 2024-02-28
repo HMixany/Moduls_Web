7f73c610e60178ce6d57bad847e9048a61d89584
HMixany <hmixany@gmail.com>
2024-02-28 23:53:30 +0200

    second commit

--- Modul_12/ex_01/src/routes/auth.py
+++ Modul_12/ex_01/src/routes/auth.py
@@ -5,14 +5,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
 from src.repository import users as repositories_users
 from src.schemas.user import UserSchema, TokenSchema, UserResponse
+from src.services.auth import auth_service
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/signup")
+@router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserSchema, db: AsyncSession = Depends(get_db)):
-    pass
-    return {}
+    exist_user = await repositories_users.get_user_by_email(body.email, db)
+    if exist_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
+    body.password = auth_service.get_password_hash(body.password)
+    new_user = await repositories_users.create_user(body, db)
+    return new_user
 
 
 @router.post("/login")
